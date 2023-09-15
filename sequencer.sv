1d180ceaee51c257e986bc52addd8fd05192d685
Srivathsa2304 <103250421+Srivathsa2304@users.noreply.github.com>
2023-09-15 10:00:43 +0530

    Update sequencer.sv

--- sequencer.sv
+++ sequencer.sv
@@ -1,1 +1,7 @@
+class fifo_sequencer extends uvm_sequencer #(fifo_sequence_items);
+  `uvm_component_utils(fifo_sequencer)
 
+  function new(string name="fifo_sequencer", uvm_component parent);
+    super.new(name,parent);
+  endfunction
+endclass
